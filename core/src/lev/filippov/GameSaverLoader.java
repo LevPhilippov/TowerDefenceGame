1079804dd0f1eb61ce6192c3e426dd8180649d8b
LevPhilippov <filippov.l.a.89@gmail.com>
2019-04-13 17:43:47 +0700

    Наладил загрузку-сохранение для .jar и проекта в Idea. Используется абсолютный путь к каталогу. Карты и сейвы располагаются в том же каталоге что и исполняемый файл(для .jar) или в папке productions для несобранного проекта.

--- core/src/lev/filippov/GameSaverLoader.java
+++ core/src/lev/filippov/GameSaverLoader.java
@@ -8,24 +8,17 @@ import java.util.*;
 
 public class GameSaverLoader {
 
-//    public static void saveProgress(String playerName) {
-//
-//        String mapName = saveCompletedRound(playerName);
-//        try {
-//            Writer writer = Gdx.files.external("saves/" + playerName + ".save").writer(false);
-//            writer.write(mapName);
-//            writer.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+    final static String path;
+
+    static {
+        path = findAbsolutePathToDirectory();
+    }
+
 
     public static boolean createNewPlayer(String playerName) {
-        if(!Gdx.files.local("saves/" + playerName + ".save").exists()) {
+        if(!Gdx.files.absolute(path + "saves/" + playerName + ".save").exists()) {
             try {
-                return Gdx.files.local("saves/" + playerName + ".save").file().createNewFile();
+                return Gdx.files.absolute(path + "saves/" + playerName + ".save").file().createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -36,16 +29,7 @@ public class GameSaverLoader {
     // "./maps/" - определяет путь к файлу отталкиваясь от директории в которой расположен .jar
     // или использовать new File(GameSaverLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
     public static ArrayList<String> mapListLoader() {
-        File jarFile = null;
-        try {
-            jarFile = new File(GameSaverLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        String inputFilePath = jarFile.getParent() + File.separator +"maps" + File.separator;
-
-//        String path = "./maps/";
-        File[] files = new File(inputFilePath).listFiles();
+        File[] files = new File(path+"/maps").listFiles();
         ArrayList<String> maps = new ArrayList<String>();
         for (int i = 0; i <files.length ; i++) {
             maps.add(files[i].getName());
@@ -54,23 +38,19 @@ public class GameSaverLoader {
         return maps;
     }
 
-    public static void saveCompletedRound(String playerName, String mapName) {
-//        BufferedReader bufferedReader = Gdx.files.external("saves/"+playerName+".save").reader(8192);
-//        String str = null;
-//        try {
-//            str = bufferedReader.readLine();
-//            bufferedReader.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        ArrayList<String> list = mapListLoader();
-//
-//        if(str==null) {
-//            return list.get(0);
-//        }
-//        return list.get(list.indexOf(str)+1);
+    private static String findAbsolutePathToDirectory() {
+        File jarFile = null;
+        try {
+            jarFile = new File(GameSaverLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return jarFile.getParent() + File.separator;
+    }
 
-        Writer writer = Gdx.files.local("saves/"+playerName+".save").writer(true);
+    public static void saveCompletedRound(String playerName, String mapName) {
+
+        Writer writer = Gdx.files.absolute(path + "saves/"+playerName+".save").writer(true);
         try {
             writer.write("\n");
             writer.write(mapName);
@@ -81,7 +61,7 @@ public class GameSaverLoader {
     }
 
     public static String loadGame (String playerName) {
-        BufferedReader bufferedReader = Gdx.files.local("saves/"+playerName+".save").reader(8192);
+        BufferedReader bufferedReader = Gdx.files.absolute(path + "saves/"+playerName+".save").reader(8192);
         String str = null;
         ArrayList<String> mapList = new ArrayList<String>();
         try {
